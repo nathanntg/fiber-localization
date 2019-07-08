f420af5340e8d3adda252f6590705245c044a792
Nathan Perkins <nathan@maxmo.net>
2019-07-08 14:05:51 -0400

    Start FindFibers class architecture

--- measure/FindFibers.m
+++ measure/FindFibers.m
@@ -17,13 +17,12 @@ classdef FindFibers
         function CL = FindFibers()
             %FINDFIBERS Construct an instance of this class
             %   Detailed explanation goes here
-            obj.Property1 = inputArg1 + inputArg2;
         end
         
-        function outputArg = method1(obj,inputArg)
+        function success = findFibers(CL, camera)
             %METHOD1 Summary of this method goes here
             %   Detailed explanation goes here
-            outputArg = obj.Property1 + inputArg;
+            
         end
     end
 end
